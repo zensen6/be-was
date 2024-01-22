ec5c1b0488ed64fca548a3935a66be432a0517da
zensen6 <junhee1469@gmail.com>
2024-01-22 12:30:39 +0900

    signup with post

--- src/main/java/Controller/User/UserController.java
+++ src/main/java/Controller/User/UserController.java
@@ -42,57 +42,49 @@ public class UserController {
         Response response = new Response();
         try {
             if ("form.html".equals(URI)) {
-                //body = Files.readAllBytes(new File(filePath + "/user/form.html").toPath());
                 body = FileBytes.FilesreadAllBytes(filePath + "/user/form.html");
                 response.SetHttpStatus(HttpStatus.OK);
 
             }else if("login".equals(URI)){
                 String body_str = request.GetBody();
-                logger.debug(body_str);
                 String userId = body_str.split("&")[0].split("=")[1];
                 String password = body_str.split("&")[1].split("=")[1];
 
-                logger.debug("userId:" + userId);
-                logger.debug("password:" + password);
                 User user = Database.findUserById(userId);
                 if(user != null && user.getPassword().equals(password)){ // 로그인 성공
-                    logger.debug("logged in");
-                    //body = Files.readAllBytes(new File(filePath + "/index.html").toPath());
                     body = FileBytes.FilesreadAllBytes(filePath + "/index.html");
                     response.SetRedirectUrl(HttpStatus.REDIRECT, "/index.html");
                 }else{
-                    //body = Files.readAllBytes(new File(userFile + "/login_failed.html").toPath());
                     body = FileBytes.FilesreadAllBytes(userFile + "/login_failed.html");
-
                     response.SetHttpStatus(HttpStatus.OK);
                 }
 
             }else if("create".equals(URI.substring(0,6))){
 
-                String []args = URI.split("&");
-                String userId = args[0].split("\\?")[1].split("=")[1];
-                String password = args[1].split("=")[1];
-                String name = args[2].split("=")[1];
-                String email = args[3].split("=")[1];
-                User user = new User(userId, password, name, email);
+                String body_str = request.GetBody();
 
+                logger.debug(body_str);
+                String userId = body_str.split("&")[0].split("=")[1];
+                String password = body_str.split("&")[1].split("=")[1];
+                String name = body_str.split("&")[2].split("=")[1];
+                String email = body_str.split("&")[3].split("=")[1];
                 User finduser = Database.findUserById(userId);
                 if(finduser == null){
+                    User user = new User(userId, password, name, email);
                     Database.addUser(user);
-                    //body = Files.readAllBytes(new File(userFile + "/login.html").toPath());
                     body = FileBytes.FilesreadAllBytes(userFile + "/login.html");
-
 
                     response.SetRedirectUrl(HttpStatus.REDIRECT, "/user/login.html");
 
                 }else{
-                    //body = Files.readAllBytes(new File(userFile + "/signup_failed.html").toPath());
+
                     body = FileBytes.FilesreadAllBytes(userFile + "/signup_failed.html");
                     response.SetHttpStatus(HttpStatus.OK);
                 }
 
+
+
             }else if("login.html".equals(URI)){
-                //body = Files.readAllBytes(new File(filePath + "/user/login.html").toPath());
                 body = FileBytes.FilesreadAllBytes(filePath + "/user/login.html");
                 response.SetHttpStatus(HttpStatus.OK);
 
